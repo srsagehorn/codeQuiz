99bed4582b8e22312848acdfa901315b205b24de
Shannon Sagehorn <srsagehorn@gmail.com>
2020-06-02 23:22:49 -0500

    set up basicc html layout and pseudo code

---
+++ script.js
@@ -0,0 +1,32 @@
+var questions = [
+  "Commonly used data types DO NOT include:",
+  "The condition in an if / else statement is enclosed within_______.",
+  "Arrays in JavaScript can be used to store ________.",
+  "String values must be enclosed within _______ when being assigned to variables.",
+  "A very useful tool used during development and debugging for printing conttent to the debugger is:",
+];
+
+var ans0 = ["1. strings", "2. booleans", "3. alerts", "4. numbers"];
+var ans1 = [
+  "1. quotes",
+  "2. curly brackets",
+  "3. parentheses",
+  "4. square brackets",
+];
+var ans2 = [
+  "1. numbers and strings",
+  "2. other arrays",
+  "3. booleans",
+  "4. all of the above",
+];
+var ans3 = ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"];
+var ans4 = [
+  "1. JavaScript",
+  "2. terminal/bash",
+  "3. for loops",
+  "4. console log",
+];
+
+$("opener");
+if ($("opener")) {
+}
